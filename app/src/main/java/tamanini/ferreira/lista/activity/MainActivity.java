92b72f5fbe9f85cdfa6710194c54843334d80186
20221tiimi0289 <annajulia.tamanini@gmail.com>
2024-04-19 09:09:16 -0300

    Codigo

--- app/src/main/java/tamanini/ferreira/lista/activity/MainActivity.java
+++ app/src/main/java/tamanini/ferreira/lista/activity/MainActivity.java
@@ -1,6 +1,8 @@
-package tamanini.ferreira.lista;
+package tamanini.ferreira.lista.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import tamanini.ferreira.lista.R;
+
 public class MainActivity extends AppCompatActivity {
+
+    static int NEW_ITEM_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,16 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        FloatingActionButton fabAddItem = findViewById(R.id.fabAddNewItem);
+        fabAddItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,NewItemActivity.class);
+                startActivityForResult(i, NEW_ITEM_REQUEST);
+
+            }
+        });
+
+
     }
 }
